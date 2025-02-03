f366a9c4d4adbd82c1818330488dc02c82f5a0ad
Ilya <otkritie222@gmail.com>
2025-02-03 14:47:18 +0300

    2.6

---
+++ 2.6/Lab26_LogNorm-student/app/src/main/java/ru/myitschool/lab23/MainActivity.kt
@@ -0,0 +1,35 @@
+package ru.myitschool.lab23
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ru.myitschool.lab23.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    lateinit var viewModel : MyViewModel
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding  = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        viewModel = ViewModelProvider(this).get(MyViewModel::class.java)
+        observeViewModel()
+        binding.content.getRandomNum.setOnClickListener{
+            viewModel.result.value = viewModel.generateLogNormal()
+        }
+    }
+    fun observeViewModel(){
+        viewModel.mu.observe(this, Observer{
+            binding.content.meanVal.setText(it.toString())
+        })
+        viewModel.sigma2.observe(this, Observer{
+            binding.content.varianceValue.setText(it.toString())
+        })
+        viewModel.result.observe(this, Observer{
+            binding.content.randomValueResult.text = it.toString()
+        })
+
+    }
+}
---
+++ 2.6/Lab26_LogNorm-student/app/src/main/java/ru/myitschool/lab23/MyViewModel.kt
@@ -0,0 +1,18 @@
+package ru.myitschool.lab23
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import kotlin.math.exp
+import kotlin.math.sqrt
+
+class MyViewModel : ViewModel() {
+    val mu = MutableLiveData<Double>().apply { value = 0.0 }
+    val sigma2 = MutableLiveData<Double>().apply { value = 1.0 }
+    val result = MutableLiveData<Double>()
+    fun generateLogNormal(): Double {
+        val sigma = sqrt((sigma2.value ?: 1.0))
+        val random = java.util.Random() // Используем java.util.Random
+        val normalValue = random.nextGaussian() // Генерация нормального числа
+        return exp((mu.value ?: 0.0) + sigma * normalValue) // Преобразование в логнормальное
+    }
+}
