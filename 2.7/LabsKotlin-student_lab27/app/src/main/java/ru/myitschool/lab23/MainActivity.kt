1d7e6edf496473e2bdba1353b738debe7dce43d7
Ilya <otkritie222@gmail.com>
2025-02-04 20:51:32 +0300

    2.7

---
+++ 2.7/LabsKotlin-student_lab27/app/src/main/java/ru/myitschool/lab23/MainActivity.kt
@@ -0,0 +1,26 @@
+package ru.myitschool.lab23
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import ru.myitschool.lab23.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding :ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.content.showAction.setOnClickListener{
+            binding.content.summaryText.apply {
+                val checkedStates = listOf(
+                    binding.content.chipMaterial?.let { "canonicalname1:${it.isChecked}" },
+                ).filterNotNull().joinToString()
+                text = checkedStates
+            }
+        }
+    }
+
+
+}
